56319f062cb7e25af62999caca7350ee80fe85f0
asscat1337 <47982970+asscat1337@users.noreply.github.com>
2020-09-24 16:39:59 +0500

    Add files via upload

---
+++ auth/authPage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/auth.css">
+</head>
+<body> 
+    <div class="auth__wrapper">
+    <div class="auth__content">
+    <form class="form__auth">
+        <label for="login">Логин</label>
+        <input type="text" name="login" class="login">
+        <label for="pass">Пароль</label>
+        <input type="password" name="pass" class="pass">
+        <button class="btnOk">Авторизация</button>
+    </form>
+    </div>
+    </div>
+    <script src="../js/authorization.js"></script>
+</body>
+</html>
